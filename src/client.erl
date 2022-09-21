beebed038665b120e588637065b4ca62cf3206b6
Christy George <christygeorgeind@gmail.com>
2022-09-20 20:08:25 -0400

    Add collector

--- src/client.erl
+++ src/client.erl
@@ -9,4 +9,11 @@ call_server(User, IP)->
     
     SuccessValue=net_kernel:connect_node('chris@127.0.0.1'),
     io:fwrite("Connection : ~w",[SuccessValue]),
-    server:ping().
+
+    {serverProcess, 'chris@127.0.0.1'} ! {self(),node()},
+    io:fwrite("Yayyyyyyy : ~w",[SuccessValue]),
+    receive
+        HashZero ->
+            io:fwrite("~nCLIENT PINGED!!! Got HASHZERO ~p",[HashZero]),
+            server:start_actors(node(),HashZero,10,1000000)
+    end.
--- src/server.erl
+++ src/server.erl
@@ -1,31 +1,19 @@
 -module(server).
--export([ping/0,server_start/1]).
+-export([start_actors/4,server_start/1,connect_worker/1,collector/0]).
 -import_module([mine]).
 
-ping()->
-    io:fwrite("~nCLIENT PINGED!!!"),
-    ServerProcess = registered(),
-    io:fwrite("~n Found Serverprocess ~w",[ServerProcess]),
-    ServerProcess ! self(),
-    
-    receive
-        {HashZero} ->
-            start_actors(node(),HashZero,10,1000000)
-    end.
-
-start_actors(_, _,ActorCount,CoinCount) when ActorCount ==0 ->
-     io:fwrite("~n~w Coins mined",[CoinCount]),
+start_actors(_, _,ActorCount,_) when ActorCount ==0 ->
+    %  io:fwrite("~n~w Coins mined",[CoinCount]),
      exit;
 
 start_actors(Node, HashZero, ActorCount, CoinCount) when ActorCount > 0  ->
-    io:fwrite("~nNode ~w",[Node]),
     Pid = spawn(Node, mine, mine_coin, []),
-    io:fwrite("Actor ~w spawned by ~w", [Pid,self()]),
+    % io:fwrite("Actor ~w spawned by ~w", [Pid,self()]),
     Pid ! {self(), HashZero},
     receive
         {Pid, Status, RandomString, HashString}->
             if(Status == true) ->
-                io:fwrite("~nRandom String:~s Coin:~s",[RandomString, HashString]),
+                {collectorProcess, 'chris@127.0.0.1'} ! {node(),RandomString, HashString},
                 start_actors(Node,HashZero, ActorCount - 1, CoinCount - 1)
             ; (Status == false) ->
                 Pid ! {self()}
@@ -33,12 +21,26 @@ start_actors(Node, HashZero, ActorCount, CoinCount) when ActorCount > 0  ->
     end,
     start_actors(Node, HashZero, ActorCount - 1, CoinCount).
 
-server_start(HashZero) ->
-    register(serverProcess, self()),
-    start_actors(node(),HashZero,10,1000000),
-
+connect_worker(HashZero)->
+    io:fwrite("~nWAITING FOR WORKER..."),
     receive
-        {Worker} -> 
+        {Worker,Node} -> %BUT WHY!!!!!
+            io:fwrite("~nCLIENT CONNECTED... ~w ~w",[Worker,Node]),
             Worker ! HashZero
-    end.
+    end,
+    connect_worker(HashZero).
+
+collector()->
+    receive
+        {Node, RandomString, HashString}->
+            io:fwrite("~nNode ~w Random String:~s Coin:~s",[Node,RandomString, HashString])
+    end,
+    collector().
+
+server_start(HashZero) ->
+    io:fwrite("~nStarting Server..."),
+    register(serverProcess,spawn(node(),server,connect_worker,[HashZero])),
+    register(collectorProcess,spawn(node(),server,collector,[])),
+    start_actors(node(),HashZero,10,14).
+
     
