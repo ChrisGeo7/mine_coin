49a0e925d97262d42465158e92fbb4c83412231c
Aiswarya <aiswaryawarrier96@gmail.com>
2022-09-23 03:22:44 +0530

    Passing node as input for client

--- src/client.erl
+++ src/client.erl
@@ -1,16 +1,14 @@
 -module(client).
--export([call_server/2]).
+-export([call_server/1]).
 -import_module(server).
 
 
-call_server(User, IP)->
-    HostName=User++"@"++IP,
-    io:fwrite("~s~n",[HostName]),
-    net_kernel:connect_node('chris@127.0.0.1'),
-
-    {serverProcess, 'chris@127.0.0.1'} ! {self(),node()},
+call_server(User)->
+    io:fwrite("Connecting to ~s~n",[User]),
+    net_kernel:connect_node(User),
+    {serverProcess, User} ! {self(),node()},
     receive
         HashZero ->
-            io:fwrite("~nCLIENT PINGED!!! Got HASHZERO ~p",[HashZero]),
-            main:spawn_actors(node(),HashZero,10)
+            io:fwrite("~n Connected to server, Starting to mine, zeroCount : ~p",[HashZero]),
+            mine:spawn_actors(node(),HashZero,10)
     end.
